ac599f6059535cf6857ed1a256dd5d329173956d
Marco Ieni <11428655+MarcoIeni@users.noreply.github.com>
2023-05-27 12:32:39 +0200

    select on enter

--- src/glimpse.ts
+++ src/glimpse.ts
@@ -9,7 +9,7 @@ export async function glimpseRun(context: vscode.ExtensionContext) {
 function pick(glimpses: Map<string, Key>) {
     const quickPick = vscode.window.createQuickPick();
 
-    // fill quick pick options
+    // Fill quick pick options.
     let options = [];
     for (const [key, value] of glimpses.entries()) {
         options.push({
@@ -19,22 +19,41 @@ function pick(glimpses: Map<string, Key>) {
     }
 
     quickPick.items = options;
+    // Allow to select action by key press.
     quickPick.onDidChangeValue(() => {
-        console.log("user typed ", quickPick.value);
-        if (quickPick.value.length !== 0) {
-            const key = quickPick.activeItems[0].label;
-            const command = glimpses.get(key)?.command;
-            if (command) {
-                if (typeof command === "string") {
-                    vscode.commands.executeCommand(command);
-                } else if (command instanceof Map) {
-                    pick(command);
-                }
-
-                quickPick.dispose();
-            }
-        }
+        onValueChange(quickPick, glimpses);
+    });
+    // Allow to select action with enter key.
+    quickPick.onDidAccept(() => {
+        executeGlimpse(quickPick, glimpses);
     });
     quickPick.onDidHide(() => quickPick.dispose());
     quickPick.show();
+}
+
+function onValueChange(
+    quickPick: vscode.QuickPick<vscode.QuickPickItem>,
+    glimpses: Map<string, Key>
+) {
+    console.log("user typed ", quickPick.value);
+    if (quickPick.value.length !== 0) {
+        executeGlimpse(quickPick, glimpses);
+    }
+}
+
+function executeGlimpse(
+    quickPick: vscode.QuickPick<vscode.QuickPickItem>,
+    glimpses: Map<string, Key>
+) {
+    const key = quickPick.activeItems[0].label;
+    const command = glimpses.get(key)?.command;
+    if (command) {
+        if (typeof command === "string") {
+            vscode.commands.executeCommand(command);
+        } else if (command instanceof Map) {
+            pick(command);
+        }
+
+        quickPick.dispose();
+    }
 }
