34933142cf73e9e0b578c69207fc53cea9ba8a77
Marco Ieni <11428655+MarcoIeni@users.noreply.github.com>
2023-05-29 22:39:54 +0200

    fmt

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,7 @@ module.exports = {
     parserOptions: {
         ecmaVersion: 6,
         sourceType: "module",
-        project: './tsconfig.json',
+        project: "./tsconfig.json",
     },
     extends: [
         "eslint:recommended",
