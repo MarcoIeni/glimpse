f99da8129d0d600b80c9fee15f76f1e6fb9841d0
Marco Ieni <11428655+MarcoIeni@users.noreply.github.com>
2023-07-05 22:03:14 +0200

    new url

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
     favicon: "img/favicon.ico",
 
     // Set the production url of your site here
-    url: "https://marcoieni.github.io",
+    url: "https://glimpse.ieni.dev",
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: "/glimpse/",
+    baseUrl: "/",
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
