df1f9da11fa4346b9a723663730db3946a25748d
Marco Ieni <11428655+MarcoIeni@users.noreply.github.com>
2023-07-24 17:38:09 +0200

    wip

--- src/keys.ts
+++ src/keys.ts
@@ -3,6 +3,7 @@ import { configPath, pathExists } from "./config";
 import { defaultMenu } from "./keys/default_menu";
 import { type Icon } from "./icons";
 import { Logger, notifyError } from "./logger";
+import { utf8ArrayToStr } from "./utf8ToStr";
 
 export type Key = CommandOrSubmenu & KeyDescription;
 
@@ -79,9 +80,9 @@ export async function menu(context: vscode.ExtensionContext): Promise<Menu> {
         return fromUserMenu(originalMenu);
     }
     try {
-        // read the content of configFilePath
         await vscode.workspace.fs.readFile(configFilePath).then((content) => {
-            const fileContent =utf8ArrayToStr(content);
+            // read the content of configFilePath
+            const fileContent = utf8ArrayToStr(content);
             Logger.debug("config file content: " + fileContent);
             // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
             const userModule = eval(fileContent);
@@ -96,56 +97,6 @@ export async function menu(context: vscode.ExtensionContext): Promise<Menu> {
         notifyError(msg);
         return fromUserMenu(originalMenu);
     }
-}
-
-// http://www.onicos.com/staff/iz/amuse/javascript/expert/utf.txt
-
-/* utf.js - UTF-8 <=> UTF-16 convertion
- *
- * Copyright (C) 1999 Masanao Izumo <iz@onicos.co.jp>
- * Version: 1.0
- * LastModified: Dec 25 1999
- * This library is free.  You can redistribute it and/or modify it.
- */
-function utf8ArrayToStr(array: Uint8Array): string {
-    let out, i, c;
-    let char2, char3;
-
-    out = "";
-    const len = array.length;
-    i = 0;
-    while (i < len) {
-        c = array[i++];
-        switch (c >> 4) {
-            case 0:
-            case 1:
-            case 2:
-            case 3:
-            case 4:
-            case 5:
-            case 6:
-            case 7:
-                // 0xxxxxxx
-                out += String.fromCharCode(c);
-                break;
-            case 12:
-            case 13:
-                // 110x xxxx   10xx xxxx
-                char2 = array[i++];
-                out += String.fromCharCode(((c & 0x1f) << 6) | (char2 & 0x3f));
-                break;
-            case 14:
-                // 1110 xxxx  10xx xxxx  10xx xxxx
-                char2 = array[i++];
-                char3 = array[i++];
-                out += String.fromCharCode(
-                    ((c & 0x0f) << 12) | ((char2 & 0x3f) << 6) | ((char3 & 0x3f) << 0),
-                );
-                break;
-        }
-    }
-
-    return out;
 }
 
 function fromUserMenu(userMenu: UserMenu): Menu {
