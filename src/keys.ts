25d992b04db14eedaed22b4358f7d6deb8c6265a
Marco Ieni <11428655+MarcoIeni@users.noreply.github.com>
2023-07-24 17:04:53 +0200

    fix vscode web config

--- src/keys.ts
+++ src/keys.ts
@@ -1,8 +1,8 @@
-import type * as vscode from "vscode";
+import * as vscode from "vscode";
 import { configPath, pathExists } from "./config";
 import { defaultMenu } from "./keys/default_menu";
 import { type Icon } from "./icons";
-import { notifyError } from "./logger";
+import { Logger, notifyError } from "./logger";
 
 export type Key = CommandOrSubmenu & KeyDescription;
 
@@ -81,12 +81,21 @@ const requireDynamically = (path: string) =>
 export async function menu(context: vscode.ExtensionContext): Promise<Menu> {
     const originalMenu = defaultMenu();
     const configFilePath = configPath(context);
-    if (!(await pathExists(configFilePath))) {
+    const configExists = await pathExists(configFilePath);
+    Logger.info("config exists " + configExists);
+    Logger.info("path: " + configFilePath.fsPath + " vs " + configFilePath.toString());
+
+    if (!configExists) {
         return fromUserMenu(originalMenu);
     }
     try {
+        // read the content of configFilePath
+        await vscode.workspace.fs.readFile(configFilePath).then((content) => {
+            Logger.debug("config file content: " + utf8ArrayToStr(content));
+        });
+
         // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        const userModule = await requireDynamically(configFilePath.fsPath);
+        const userModule = await requireDynamically(configFilePath.toString());
         // eslint-disable-next-line @typescript-eslint/no-unsafe-call
         const userSpecificMenu = userModule(originalMenu) as UserMenu;
         return fromUserMenu(userSpecificMenu);
@@ -96,6 +105,56 @@ export async function menu(context: vscode.ExtensionContext): Promise<Menu> {
         notifyError(msg);
         return fromUserMenu(originalMenu);
     }
+}
+
+// http://www.onicos.com/staff/iz/amuse/javascript/expert/utf.txt
+
+/* utf.js - UTF-8 <=> UTF-16 convertion
+ *
+ * Copyright (C) 1999 Masanao Izumo <iz@onicos.co.jp>
+ * Version: 1.0
+ * LastModified: Dec 25 1999
+ * This library is free.  You can redistribute it and/or modify it.
+ */
+function utf8ArrayToStr(array: Uint8Array): string {
+    let out, i, c;
+    let char2, char3;
+
+    out = "";
+    const len = array.length;
+    i = 0;
+    while (i < len) {
+        c = array[i++];
+        switch (c >> 4) {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+                // 0xxxxxxx
+                out += String.fromCharCode(c);
+                break;
+            case 12:
+            case 13:
+                // 110x xxxx   10xx xxxx
+                char2 = array[i++];
+                out += String.fromCharCode(((c & 0x1f) << 6) | (char2 & 0x3f));
+                break;
+            case 14:
+                // 1110 xxxx  10xx xxxx  10xx xxxx
+                char2 = array[i++];
+                char3 = array[i++];
+                out += String.fromCharCode(
+                    ((c & 0x0f) << 12) | ((char2 & 0x3f) << 6) | ((char3 & 0x3f) << 0),
+                );
+                break;
+        }
+    }
+
+    return out;
 }
 
 function fromUserMenu(userMenu: UserMenu): Menu {
