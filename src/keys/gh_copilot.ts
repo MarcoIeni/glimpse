954258b8deea58b96db5d788ca96f720d89affb5
Marco Ieni <11428655+MarcoIeni@users.noreply.github.com>
2023-07-27 11:48:09 +0200

    fix

--- src/keys/gh_copilot.ts
+++ src/keys/gh_copilot.ts
@@ -1,56 +1,74 @@
-import * as vscode from "vscode";
+/// I can't simply import vscode because when building the keybindings.md file, vscode is not available.
+let vscode: { extensions: { all: [{ id: string }] } } | null = null;
+try {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    vscode = require("vscode");
+} catch (error) {
+    console.log('The "vscode" module is not available.');
+}
 import { type UserMenu } from "../keys";
 
+function isGhCopilotPresent(): boolean {
+    if (vscode) {
+        const extensions = vscode.extensions.all;
+        for (const ext of extensions) {
+            if (ext.id === "GitHub.copilot-chat") {
+                return true;
+            }
+        }
+    } else {
+        // If vscode is not available, it means we are building the keybindings.md file for the docs.
+        return true;
+    }
+    return false;
+}
 /**
  * Returns an object containing the key bindings for GitHub Copilot.
  */
 export function ghCopilotKeys(): UserMenu | null {
-    const extensions = vscode.extensions.all;
-    for (const ext of extensions) {
-        if (ext.id === "GitHub.copilot-chat") {
-            return {
-                items: [
-                    {
-                        key: "c",
-                        name: "Copilot Chat",
-                        icon: "comment",
-                        command: "workbench.panel.chatSidebar.copilot",
-                    },
-                    {
-                        key: "e",
-                        name: "Explain this",
-                        icon: "info",
-                        command: "github.copilot.interactiveEditor.explain",
-                    },
-                    {
-                        key: "f",
-                        name: "Fix this",
-                        icon: "lightbulb",
-                        command: "github.copilot.interactiveEditor.fix",
-                    },
-                    {
-                        key: "g",
-                        name: "Generate",
-                        icon: "beaker",
-                        menu: generate,
-                    },
-                    {
-                        key: "o",
-                        name: "Open Completions Panel",
-                        icon: "octoface",
-                        command: "github.copilot.generate",
-                    },
-                    {
-                        key: "t",
-                        name: "Toggle (Enable/Disable) Copilot",
-                        icon: "circle-slash",
-                        command: "github.copilot.toggleCopilot",
-                    },
-                ],
-            };
-        }
+    if (!isGhCopilotPresent()) {
+        return null;
     }
-    return null;
+    return {
+        items: [
+            {
+                key: "c",
+                name: "Copilot Chat",
+                icon: "comment",
+                command: "workbench.panel.chatSidebar.copilot",
+            },
+            {
+                key: "e",
+                name: "Explain this",
+                icon: "info",
+                command: "github.copilot.interactiveEditor.explain",
+            },
+            {
+                key: "f",
+                name: "Fix this",
+                icon: "lightbulb",
+                command: "github.copilot.interactiveEditor.fix",
+            },
+            {
+                key: "g",
+                name: "Generate",
+                icon: "beaker",
+                menu: generate,
+            },
+            {
+                key: "o",
+                name: "Open Completions Panel",
+                icon: "octoface",
+                command: "github.copilot.generate",
+            },
+            {
+                key: "t",
+                name: "Toggle (Enable/Disable) Copilot",
+                icon: "circle-slash",
+                command: "github.copilot.toggleCopilot",
+            },
+        ],
+    };
 }
 
 const generate: UserMenu = {
---
+++ website/docs/keybindings.md
@@ -0,0 +1,89 @@
+---
+sidebar_position: 6
+---
+## Configuration
+
+## Comment
+
+## Debug
+
+## Diff/Compare
+
+## Error
+
+## Editor
+
+## File
+
+## Format
+
+## Git
+
+## Help
+
+## Insert
+
+## Indent
+
+## Jump
+
+## Project
+
+## Peek
+
+## Quit
+
+## Refactor
+
+## Search
+
+## Show
+
+## Toogle
+
+## Test
+
+## Window/Group
+
+## Text
+
+## Select/expand region
+
+## Zoom/Fold
+
+## Commands
+
+## Last editor
+
+## Show terminal
+
+## Open new external terminal
+
+## Show terminal
+
+## Search Selection in Project
+
+## Search in project
+
+## Task
+
+## Toggle comment
+
+## Focus on files explorer
+
+## Focus 1st editor group
+
+## Focus 2nd editor group
+
+## Focus 3rd editor group
+
+## Focus 4th editor group
+
+## Focus 5th editor group
+
+## Focus 6th editor group
+
+## Focus 7th editor group
+
+## Focus 8th editor group
+
