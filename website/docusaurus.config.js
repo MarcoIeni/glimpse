54d63fba8a1253659b95b2ef5c8ab6ff70c6313c
Marco Ieni <11428655+MarcoIeni@users.noreply.github.com>
2023-08-01 16:33:23 +0200

    docs: fix edit url

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -59,7 +59,7 @@ const config = {
                     sidebarPath: require.resolve("./sidebars.js"),
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
-                    editUrl: "https://github.com/MarcoIeni/glimpse/tree/main/docs/",
+                    editUrl: "https://github.com/MarcoIeni/glimpse/tree/main/website/",
                 },
                 theme: {
                     customCss: require.resolve("./src/css/custom.css"),
