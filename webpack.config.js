6b2fe3aef376a210d0c32ec78fbfada319cc5083
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2024-06-24 09:30:53 +0200

    remove default tests

--- webpack.config.js
+++ webpack.config.js
@@ -78,9 +78,6 @@ const nodeConfig = /** @type WebpackConfig */ {
     target: "node", // extensions run in a node context
     entry: {
         "extension-node": "./src/extension.ts", // source of the node extension main file
-        "test/suite/index-node": "./src/test/suite/index-node.ts", // source of the node extension test runner
-        "test/suite/extension.test": "./src/test/suite/extension.test.ts", // create a separate file for the tests, to be found by glob
-        "test/runTest": "./src/test/runTest", // used to start the VS Code test runner (@vscode/test-electron)
     },
     output: {
         filename: "[name].js",
